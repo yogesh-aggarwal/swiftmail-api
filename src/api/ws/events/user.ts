63abc18a8ac24507aea5a0db2d1d6ecc5cf4cac1
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-18 20:17:02 +0530

    Added event to listen to user in ws api

--- src/api/ws/events/user.ts
+++ src/api/ws/events/user.ts
@@ -1,31 +1,54 @@
-import { FIRESTORE } from "@/firebase"
+import { UserDBModel } from "@/models/user"
+import mongoose from "mongoose"
 import { WSBaseEvent } from "./event"
 
 export class WSUserEvent extends WSBaseEvent {
-	private userSubscription: () => void | null = null
+	private userChangeStream: mongoose.mongo.ChangeStream | null = null
 
 	async on() {
-		this.socket.on("user", () => {
-			this.userSubscription = FIRESTORE.collection("Users")
-				.doc(this.userID)
-				.onSnapshot((doc) => {
-					if (!doc.exists) {
-						return null
-					}
+		this.socket.on("user", async () => {
+			// Initial user data emit
+			const initialUser = await UserDBModel.find({ id: this.userID })
+				.limit(1)
+				.exec()
+			if (initialUser.length > 0) {
+				this.socket.emit("user", initialUser[0].toObject())
+			}
 
-					const user = doc.data()
-					this.socket.emit("user", user)
-				})
+			// Setup change stream to watch for updates
+			this.userChangeStream = UserDBModel.watch(
+				[
+					{
+						$match: {
+							"documentKey.id": this.userID,
+							operationType: { $in: ["update", "replace"] },
+						},
+					},
+				],
+				{ fullDocument: "updateLookup" }
+			)
+
+			this.userChangeStream.on("change", async () => {
+				const updatedUser = await UserDBModel.find({ id: this.userID })
+					.limit(1)
+					.exec()
+				if (updatedUser.length > 0) {
+					this.socket.emit("user", updatedUser[0].toObject())
+				}
+			})
 		})
 	}
 
 	async off() {
 		this.socket.off("user", () => {
-			this.userSubscription?.()
+			this.cleanup()
 		})
 	}
 
 	async cleanup() {
-		this.userSubscription?.()
+		if (this.userChangeStream) {
+			await this.userChangeStream.close()
+			this.userChangeStream = null
+		}
 	}
 }
