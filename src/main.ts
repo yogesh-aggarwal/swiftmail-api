a69eea3d7d215fb682e56aaa13f7d0552f3d9c5a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-27 14:34:40 +0530

    Updated cors origin

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,11 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: ["http://localhost:5173"] }))
+app.use(
+	cors({
+		origin: ["http://localhost:5173", "https://swiftmail-web.vercel.app"],
+	})
+)
 
 //----------------------------------------------------------------------------------------------
 // Routes
